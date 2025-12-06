027313a83dcf84edbbb711035b02bbfafc0a5a3a
Sly-J7 <118134904+Sly-J7@users.noreply.github.com>
2025-12-06 16:38:47 +0000

    Update side navigation links and their indices

--- header.js
+++ header.js
@@ -50,39 +50,40 @@ document.getElementById("main").prepend(header)
 }
 {
 var sideNav = document.getElementById("mySidenav")
-var home =sideNav.children[0];
+var home =sideNav.children[1];
 home.textContent="Home";
 home.href="https://matesa.space/"; 
 
-var proGrams =sideNav.children[1];
+var proGrams =sideNav.children[2];
 proGrams.textContent="Prgrams";
 proGrams.href="https://matesa.space/Programs.html";
 
-var articles =sideNav.children[2];
+var articles =sideNav.children[3];
 articles.textContent="Articles";
 articles.href=message;
 
-var gall =sideNav.children[3];
+var gall =sideNav.children[4];
 gall.textContent="Gallery";
 gall.href="https://matesa.space/matgallery.html";
 
-mih = sideNav.children[4];
+mih = sideNav.children[5];
 mih.textContent="The MIH";
 mih.href="https://matesa.space/MIH.html";
 
-var about =sideNav.children[5];
+var about =sideNav.children[6];
 about.textContent="About Us";
 about.href="https://matesa.space/about.html";
 
-var exe =sideNav.children[6];
+var exe =sideNav.children[7];
 exe.textContent="MATESA Executives";
 exe.href="https://matesa.unaux.com/executives.html";
 
-report = sideNav.children[7];
+report = sideNav.children[8];
 report.textContent="Reports";
 report.href=message;
 
-var cont =sideNav.children[8];
+var cont = document.createElement("a");
 cont.textContent="Contact";
 cont.href="https://matesa.space/contact.html";
+sideNav.appendChild(cont);
 }
